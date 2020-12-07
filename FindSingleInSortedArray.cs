15ceb1a2f7adc0fc51e8a0189bfd1f2eb1e0783f
Sherine Thomas <sherinethachil@gmail.com>
2020-12-06 19:02:13 -0800

    Finding single element in a sorted array

---
+++ FindSingleInSortedArray.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms
+{
+    class FindSingleInSortedArray
+    {
+        public static void findSingle(int[] arr,int low,int high)
+        {
+            if (low > high)
+            {
+                Console.WriteLine("No such element");
+                return;
+            }
+
+            if (low == high)
+            {
+                Console.WriteLine("The single element is {0}", arr[low]);
+                return;
+            }
+           
+           int mid = (low + high) / 2;
+
+           if (mid % 2 == 0)
+           {
+                if (arr[mid] == arr[mid + 1])
+                    findSingle(arr, mid + 2, high);
+                else
+                    findSingle(arr, low, mid);
+           }
+           else if(mid%2 ==1)
+           {
+                if (arr[mid] == arr[mid - 1])
+                    findSingle(arr, mid+1, high);
+                else
+                   findSingle(arr, low, mid-1);
+           }
+            
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Algorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 
+              
+            1.Find the element that appears once in a sorted array where all other elements appear twice one after another. 
+            Find that element in 0(logn) complexity. 
+             
+            Input: arr[] = {1, 1, 3, 3, 4, 5, 5, 7, 7, 8, 8} 
+            Output: 4
+            
+            */
+            
+            /*
+            
+            int[] arr = { 1,1, 3, 3, 4,5, 5, 7, 7, 8,8}; 
+            FindSingleInSortedArray.findSingle(arr,0,arr.Length-1);
+
+            */
+        }
+    }
+}
